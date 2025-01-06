c5280e62e9212b030a871a444fdb57be119a635c
PrakshaaleJain <prakshaale123@gmail.com>
2025-01-06 23:49:02 +0530

    first commit

---
+++ EasyProblem.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define lli long long int
+#define ull unsigned long long
+#define pb push_back
+
+int main(){
+    #ifndef ONLINE_JUDGE
+        freopen("input.txt", "r", stdin);
+        freopen("output.txt","w",stdout);  
+    #endif
+    
+    int  t;
+    cin >> t;
+
+    while(t--){
+        int n;
+        cin >> n;
+        cout << n - 1 << endl;  
+    }
+    
+    return 0;
+}
