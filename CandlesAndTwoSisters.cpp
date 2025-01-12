09cdf05bf763b21df7d6f3543bd74abb1add6ee1
PrakshaaleJain <prakshaale123@gmail.com>
2025-01-12 23:42:16 +0530

    first commit

---
+++ CandlesAndTwoSisters.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define lli long long int
+#define ull unsigned long long
+#define pb push_back
+
+int main(){
+    #ifndef ONLINE_JUDGE
+        freopen("input.txt", "r", stdin);
+        freopen("output.txt","w",stdout);  
+    #endif
+    
+    int  t;
+    cin >> t;
+
+    while(t--){
+        lli n;
+        cin >> n;
+
+        lli avg = n / 2;
+
+        cout << (n - 1) / 2 << endl;
+        
+    }
+    
+    return 0;
+}
